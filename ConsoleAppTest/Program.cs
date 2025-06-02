69c2486412b86300971480431d3d767d17d7395f
rafasilva9537 <rafasilva9537@gmail.com>
2025-06-02 11:15:38 -0300

    Graph with square units

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,5 +1,5 @@
-﻿using System.Text.Json;
-using NumericalMethods.Interpolation;
+﻿using NumericalMethods.Interpolation;
+using ScottPlot;
 
 namespace ConsoleAppTest;
 
@@ -8,13 +8,13 @@ internal class Program
     internal static void Main(string[] args)
     {
         double[][] xyValues = [
-            [1, 5, 3, 9, 15],
-            [3, 8, 10, 9, 7],
+            [-1, 0, 2],
+            [4, 1, -1],
         ];
 
         List<double> xValues = [];
         List<double> yValues = [];
-        for(double i = 0; i < xyValues[0].Max(); i+=0.25)
+        for(double i = -10;  i <= 10; i+=0.25)
         {
             double interpolatedX = i;
             double interpolatedY = Lagrange.Interpolate(interpolatedX, xyValues[0], xyValues[1]);
@@ -24,9 +24,12 @@ internal class Program
 
         // var interpolatedXsYs = xValues.Zip(yValues, (f, s) => new { X = f, Y = s });
         //Console.WriteLine($"interpolatedXsYs: {JsonSerializer.Serialize(interpolatedXsYs)}");
-
-        ScottPlot.Plot myPlot = new();
+        
+        Plot myPlot = new();
+        
+        myPlot.Axes.SquareUnits();
         myPlot.Add.Scatter(xValues, yValues);
+        
         myPlot.SavePng("my-plot.png", 400, 400);
     }
 }
